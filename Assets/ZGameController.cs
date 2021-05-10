f6e8ff371f8262c432a85a08bfcb260d033c243c
Toby Cayce <tobycayce@gmail.com>
2021-05-10 12:10:48 -0700

    Attempt at Canvas
    I tried to create a dynamic system for creating the covers for each key by using text, but that apparently required Canvas, which worked extremely well and did not make me waste three hours as I decided to just use images instead when I get back to work.

---
+++ Assets/ZGameController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZGameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+	
+	void OnEnable()
+	{
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	// Function is called when the Z Game Controller should be disabled
+	void OnDisable()
+	{
+		
+	}
+}
