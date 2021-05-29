080dbd268680a20c03c081127a0b39cf9d63b618
Toby Cayce <tobycayce@gmail.com>
2021-05-29 11:11:15 -0700

    Setup for Game D
    a bit of setup for Game D (the dice one) is taking place.  I bet the cycling of each number will be easy

---
+++ Assets/DGameController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DGameController : MonoBehaviour
+{
+	public KeyCode Action = KeyCode.D;
+	public GameObject Dice1;
+	public GameObject Dice2;
+	public GameObject Dice3;
+	public GameObject Dice4;
+	public GameObject Dice5;
+	public GameObject Dice6;
+	private float switchTimer;
+	private int currentDiceNumber;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
